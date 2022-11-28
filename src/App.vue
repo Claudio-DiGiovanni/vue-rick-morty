1fb0f8b0599d8c2234add81748c4114e406d428b
Claudio-DiGiovanni <digioclaudio97@gmail.com>
2022-11-28 14:11:41 +0100

    generate card

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <pageHeader />
+    <pageMain />
+    <pageFooter />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import pageHeader from '@/components/pageHeader.vue';
+import pageMain from '@/components/pageMain.vue';
+import pageFooter from '@/components/pageFooter.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld,
+    pageHeader,
+    pageMain,
+    pageFooter,
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "~bootstrap/scss/bootstrap"
 </style>
---
+++ src/components/pageMain.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="row row-cols-4 g-5" v-if="arrCharacters">
+      <cardCharacter
+      v-for="character in arrCharacters"
+      :key="character.id"
+      :imgUrl="character.image"
+      :name="character.name"
+      :origin="character.origin.name"
+      :species="character.species"
+      />
+    </div>
+    <div v-else>Loading...</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import cardCharacter from '@/components/cardCharacters.vue';
+
+export default {
+  name: 'pageMain',
+  components: {
+    cardCharacter,
+  },
+  data() {
+    return {
+      arrCharacters: null,
+      urlApi: 'https://rickandmortyapi.com/api/character',
+    };
+  },
+  created() {
+    // scaricare i dati
+    axios.get(this.urlApi)
+      .then((axiosResponse) => {
+        this.arrCharacters = axiosResponse.data.results;
+      });
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
